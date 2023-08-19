cdb6002454ef2f8342df644740e93743aee4595f
Your Name <sakshiarora245gmail.comqqaaa git config --global user.email sakshiarora245gmail.com>
2023-08-19 16:31:25 +1000

    category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Category, Product } = require("../../models");
+// const { Category, Product, ProductTag } = require("../../models");
 
 // The `/api/categories` endpoint
 
@@ -7,28 +8,93 @@ router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      // JOIN with products, using the productTag through table
+      include: [{ model: Product, attributes: ['id', 'productName','price','stock', 'categoryId']}],
+      attributes: {
+        include:[
+          [
+            // Use plain SQL to get a count of all products
+            sequelize.literal(
+              '(SELECT COUNT(*) FROM product WHERE product.categoryId = category.id)'
+            ),
+            'numberOfProducts'
+          ]
+        ]
+      },
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // JOIN with travellers, using the Trip through table
+      include: [{ model: Product}]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No Category found with this id!" });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async(req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async(req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: "No user with this id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id",async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No location found with this id!" });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -5,31 +5,31 @@ const productData = [
     productName: "Plain T-Shirt",
     price: 14.99,
     stock: 14,
-    category_id: 1,
+    categoryId: 1,
   },
   {
     productName: "Running Sneakers",
     price: 90.0,
     stock: 25,
-    category_id: 5,
+    categoryId: 5,
   },
   {
     productName: "Branded Baseball Hat",
     price: 22.99,
     stock: 12,
-    category_id: 4,
+    categoryId: 4,
   },
   {
     productName: "Top 40 Music Compilation Vinyl Record",
     price: 12.99,
     stock: 50,
-    category_id: 3,
+    categoryId: 3,
   },
   {
     productName: "Cargo Shorts",
     price: 29.99,
     stock: 22,
-    category_id: 2,
+    categoryId: 2,
   },
 ];
 
